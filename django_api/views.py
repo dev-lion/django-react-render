3637289973cc9d35f2a0cf63a2cae37fd4e0d25c
dev-lion <alejandro_mj@outlook.com>
2025-06-12 22:58:36 -0600

    config k config

--- django_api/views.py
+++ django_api/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import View
+from django.http import HttpResponse
+import os
 
-class FrontendAppView(TemplateView):
+class FrontendAppView(View):
     template_name = "frontend/index.html"
