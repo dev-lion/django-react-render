1adda039f0f5642a367909582c4a87622ab3b106
dev-lion <alejandro_mj@outlook.com>
2025-06-12 22:24:45 -0600

    config g FrontendAppView

--- django_api/urls.py
+++ django_api/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from tasks.views import FrontendAppView  # o donde esté definida esa vista
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
