105ec3f87ba60b2c9ebcf414f332c623fe3baae9
dev-lion <alejandro_mj@outlook.com>
2025-06-12 22:17:52 -0600

    config f error coma

--- django_api/urls.py
+++ django_api/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tasks/', include('tasks.urls'))
+    path('tasks/', include('tasks.urls')),
     path('', FrontendAppView.as_view(), name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
