77df9cb7b37db1af9674e10d390a7a2538c25999
dev-lion <alejandro_mj@outlook.com>
2025-06-12 22:13:28 -0600

    config e view

--- django_api/urls.py
+++ django_api/urls.py
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('tasks/', include('tasks.urls'))
+    path('', FrontendAppView.as_view(), name='home'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
