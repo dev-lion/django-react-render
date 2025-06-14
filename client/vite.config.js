572c76f491725723b113f363f987fd49b8d2cde7
dev-lion <alejandro_mj@outlook.com>
2025-06-14 14:44:39 -0600

    vite js config desde hp

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/static/', // 👈 Esto es clave
+  build: {
+    outDir: '../django_api/frontend', // o como lo estás usando
+    emptyOutDir: true
+  }
 })
+
