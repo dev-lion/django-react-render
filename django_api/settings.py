308b2dec15514ae732c83fcfe807482ca3154d78
dev-lion <alejandro_mj@outlook.com>
2025-06-17 12:06:25 -0600

    config view  v31

--- django_api/settings.py
+++ django_api/settings.py
@@ -16,6 +16,8 @@ from pathlib import Path
 import os
 
 import mimetypes
+import dj_database_url
+
 mimetypes.add_type("application/javascript", ".js", True)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -92,10 +94,7 @@ WSGI_APPLICATION = 'django_api.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
 
 
