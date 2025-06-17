f3c5a08bfd1ffabe1bd158002d196c64e8099ccb
dev-lion <alejandro_mj@outlook.com>
2025-06-16 18:05:19 -0600

    config view  v12

--- django_api/views.py
+++ django_api/views.py
@@ -1,13 +1,7 @@
 from django.views.generic import View
-from django.http import HttpResponse, FileResponse, HttpResponseNotFound
+from django.http import HttpResponse
 from django.views.generic import TemplateView
 import os
 
-class FrontendAppView(View):
-    def get(self, request):
-        try:
-            file_path = os.path.join(os.path.dirname(__file__), 'frontend/static/index.html')
-            return FileResponse(open(file_path, 'rb'))
-        except FileNotFoundError:
-            return HttpResponseNotFound("index.html not found. Did you build the frontend?")
-
+class FrontendAppView(TemplateView):
+    template_name = 'index.html'
