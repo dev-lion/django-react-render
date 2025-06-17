8730f0e89f5b4af7b339c6f062432a3194807937
dev-lion <alejandro_mj@outlook.com>
2025-06-17 14:04:52 -0600

    build v8

--- django_api/views.py
+++ django_api/views.py
@@ -3,6 +3,9 @@ from django.http import HttpResponse, FileResponse, HttpResponseNotFound
 from django.views.generic import TemplateView
 import os
 
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.http import JsonResponse
+
 @ensure_csrf_cookie
 def set_csrf_token(request):
     return JsonResponse({'detail': 'CSRF cookie set'})
